bf139db4a9eb7e2c85f4f92275157e1c0a12eb95
omenezes20 <alissongabrieldesouza02349@gmail.com>
2026-02-07 10:53:09 -0400

    Add Next.js configuration with internationalization support

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  i18n: {
+    locales: ['en-US', 'fr', 'es'],
+    defaultLocale: 'en-US',
+  },
+};
+
+module.exports = nextConfig;
