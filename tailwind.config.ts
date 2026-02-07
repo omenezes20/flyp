d81933483e915d2750d3ae4ddf07e1a43c9599fc
omenezes20 <alissongabrieldesouza02349@gmail.com>
2026-02-07 10:53:17 -0400

    Create Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+// tailwind.config.ts
+
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+        accent: '#F5A623',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+export default config;
